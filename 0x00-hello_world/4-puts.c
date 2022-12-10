75d48cd507a5f9dc92922bf4ae14802995ba0f0e
BKHALEED <bashorunkhaleed@gmail.com>
2022-12-10 03:06:55 -0800

    Programming in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 #include <string.h>
 /*main should be void. The Beginning of the program*/
 int main(void)
-{ /*Calling a string*/
-char str1[] = "\"Programming is like building a multilingual puzzle\n";
+{/*Calling a string*/
+char str1[] = "\"Programming is like building a multilingual puzzle";
 /* writing a stdout using puts*/
 puts(str1);
  return (0);
