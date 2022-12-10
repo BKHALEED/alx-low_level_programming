597d9bdffa4e3b16c57f2263d3a7a6de835183e2
BKHALEED <bashorunkhaleed@gmail.com>
2022-12-10 06:57:20 -0800

    Programming in C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,11 @@
 */
 int main(void)
 {
-char charType;
-int integerType;
-long int longintType;
-long long longlongType;
-float floatType;
-printf("Size of a char: %Id byte",sizeof(charType));
-printf("Size of an int: %Id byte", sizeof(integerType));
-printf("Size of a long int: %Id byte",sizeof(intType));
-pritnf("Size of a long long: %Id byte",sizeof(longlongType));
-printf("Size of a float: %Id byte",sizeof(floatType));
+printf("Size of a char: %Id byte",sizeof(char));
+printf("Size of an int: %Id byte", sizeof(int));
+printf("Size of a long int: %Id byte",sizeof(lont int));
+pritnf("Size of a long long: %Id byte",sizeof(long long));
+printf("Size of a float: %Id byte",sizeof(float));
 return (0);
 }
  
