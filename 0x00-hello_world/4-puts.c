e29b97aa231f83912f0a7dabca6b2b04a81c8763
BKHALEED <bashorunkhaleed@gmail.com>
2022-12-10 02:27:26 -0800

    Programming in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <string.h>
-
+/* The Beginning of the program*/
 int main(void)
 { /*Calling a string*/
 char str1[] = "\"Programming is like building a multilingual puzzle\n";
-/* Giving puts a command*/
+/* writing a stdout using puts*/
 puts(str1);
 
-return (0);
+return 0;
 }
