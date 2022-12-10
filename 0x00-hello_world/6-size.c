0db2f11fd0cdeb0dd41e1bbaa05ef9897aea7d73
BKHALEED <bashorunkhaleed@gmail.com>
2022-12-10 05:33:49 -0800

    Programming in C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+/**
+* Main - should be void
+* Description: printing the size of integers
+* Return: 0 (Success)
+*/
+int main(void)
+{char chrtype;
+int inttype;
+longint longinttype;
+longlong longlongtype;
+float floattype;
+printf("Size of a char: %Id byte",sizeof(char));
+printf("Size of an int: %Id byte", sizeof(int));
+printf("Size of a long int: %Id byte",sizeof(long int));
+pritnf("Size of a long long: %Id byte",sizeof(long long));
+printf("Size of a float: %Id byte",sizeof(float));
+return (0);
+}
+ 
