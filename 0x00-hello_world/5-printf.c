533ae98557753bfd8499c88a7ed1e08a5d4d825d
BKHALEED <bashorunkhaleed@gmail.com>
2022-12-10 03:31:30 -0800

    Programming in C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+/**
+* main - main function
+* Description: prints out a statement without using puts
+* retuns 0 (success)
+*/
+int main()
+{printf("with proper grammar, but the outcome is a piece of art,");
+return 0;
+}
