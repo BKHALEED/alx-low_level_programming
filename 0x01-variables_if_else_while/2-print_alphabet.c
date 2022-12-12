a1cb7df529d8658102838bee970cd3a69ab1fb5c
BKHALEED <bashorunkhaleed@gmail.com>
2022-12-12 14:59:49 -0800

    Variables commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	char ch;
-	for (ch = 'a' ; ch <= 'z' ; ch++"\n")
-	{
-		putchar(ch);
-	}
-	return (0);
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+return (0);
 }
