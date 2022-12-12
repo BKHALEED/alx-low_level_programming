02ca3bf3dba03658683c609c9dcce2ebea0c276d
BKHALEED <bashorunkhaleed@gmail.com>
2022-12-12 14:42:41 -0800

    Variables commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - void
+ * description: 'writing alphabets that will prnt small ltr alp'
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
