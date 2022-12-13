887a2fc4fedff2a7bc97ec48790c58d0352bdf64
BKHALEED <bashorunkhaleed@gmail.com>
2022-12-12 17:06:18 -0800

    Variables commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main: this hould be void
+ * description: This is a print that omits q, e
+ * Return: 0 (successs
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch = 'z'; ch++)
+{
+if (ch == 'q' || ch == 'e')
+{
+continue;
+}
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
