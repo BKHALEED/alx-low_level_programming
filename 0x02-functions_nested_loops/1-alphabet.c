096423843d2cada7405b442cb0cf9a1849d4dff9
BKHALEED <bashorunkhaleed@gmail.com>
2022-12-15 18:24:22 -0800

    functions nested loops commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
  * print_alphabet - void and print out all alphabeths using putchar
- * description: print out a - z using putchar by BKHALEED
- * Return: 0 (Success)
  */
+
 void print_alphabet(void)
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
 	_putchar('\n');
