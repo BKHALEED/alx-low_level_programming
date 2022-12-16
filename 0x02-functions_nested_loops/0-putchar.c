549a28f24b2ea4d987cb90aa706183fe14d62b46
BKHALEED <bashorunkhaleed@gmail.com>
2022-12-15 17:19:25 -0800

    functions nested loops commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this is void
+ * description: write a programs that print out _putchar
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
