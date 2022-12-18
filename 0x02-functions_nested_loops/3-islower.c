1bd3e43cd0f2f1fcd0358d23f172f6818fcefcee
BKHALEED <bashorunkhaleed@gmail.com>
2022-12-17 20:02:14 -0800

    functions nested loops commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _islower - this is void
+ * description: checks for lowercase character
+ * Return: 0 (Success)
+ */
+int _islower(int c);
+{
+	int c = 'a', 'b', 'c', 'd', 'e', 'f';
+
+	if c == islower()
+		return (1);
+	else
+		return (0);
+}
