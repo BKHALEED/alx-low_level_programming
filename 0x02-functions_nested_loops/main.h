bcbffe82167cc1e65e7772fed8d0d320c80f901d
BKHALEED <bashorunkhaleed@gmail.com>
2022-12-29 20:01:54 -0800

    functions nested loops commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,5 +4,14 @@ int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
+void jack_bauer(void);
+void times_table();
+void print_to_98(int);
+void print_times_table(int);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int add(int n, int m);
+int print_last_digit(int n);
 
 #endif
