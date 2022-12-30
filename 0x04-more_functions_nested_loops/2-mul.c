bee61f2975fed00f5f5d8bd708951232f135a4ce
BKHALEED <bashorunkhaleed@gmail.com>
2022-12-30 02:31:53 -0800

    This multiply two inputs

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *mul - function that multiply two integers
+ *@a: the first input
+ *@b: the second input
+ *Return; multiplication of he two numbers
+ **/
+
+int mul(int a, int b)
+{
+		return (a * b);
+}
