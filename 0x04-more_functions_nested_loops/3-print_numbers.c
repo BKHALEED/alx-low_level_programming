7b08ca09932daabdb741f06a389ff6a82520d456
BKHALEED <bashorunkhaleed@gmail.com>
2022-12-30 20:38:12 -0800

    Print numbers from 0 -9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_numbers - print numbers 0 - 9
+ *0 -9
+ *Return: returns nothing
+ **/
+
+void print_numbers(void)
+{
+	int k = 0;
+
+	for ((k = 0 ; k < 9); k++)
+		_putchar(k);
+	_putchar("\n");
+}
