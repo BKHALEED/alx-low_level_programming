7f1c428c3d33a13783d1e38a024df4e0397408e5
BKHALEED <bashorunkhaleed@gmail.com>
2023-01-25 00:00:09 -0800

    prime number recursion commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,31 +1,30 @@
 #include "main.h"
-int prime_checker(int n, int i);
-/**
- * is_prime_number - executes prime_checker
- * @n: input to check
- * Return: Always 0 (Success)
- */
-int is_prime_number(int n)
-{
-	if (n <= 1)
-		return (0);
-	else if (prime_checker(n, n / 2) > 0)
-		return (1);
-	return (0);
-}
+#include <stdio.h>
 
 /**
- * prime_checker - checks for prime
- * @n: input to check
- * @i: n / 2, then passes to i - 1, checks if greater than 0
- * Return: prime check
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-int prime_checker(int n, int i)
+int main(void)
 {
-	if (i == 1)
-		return (1);
-	if (n % i == 0)
-		return (0);
-	else
-		return (prime_checker(n, i - 1));
+int r;
+
+r = is_prime_number(1);
+printf("%d\n", r);
+r = is_prime_number(1024);
+printf("%d\n", r);
+r = is_prime_number(16);
+printf("%d\n", r);
+r = is_prime_number(17);
+printf("%d\n", r);
+r = is_prime_number(25);
+printf("%d\n", r);
+r = is_prime_number(-1);
+printf("%d\n", r);
+r = is_prime_number(113);
+printf("%d\n", r);
+r = is_prime_number(7919);
+printf("%d\n", r);
+return (0);
 }
