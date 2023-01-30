c0db247159958b9fb6c7f8a2f5f8473d9acd84c8
BKHALEED <bashorunkhaleed@gmail.com>
2023-01-29 20:57:52 -0800

    Is a prime commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,36 +2,36 @@
 
 int check_prime(int, int);
 /**
- * * is_prime_number -> prime identifier
- * * @n: is number
- * * Return: integer
- * */
+ *is_prime_number -> prime identifier
+ *@n: is number
+ *Return: integer
+ **/
 int is_prime_number(int n)
 {
 	if (n % a == 0)
 	{
 		if (n == a)
 			return (1);
-		else 
+		else
 			return (0);
 	}
-		return (check_prime(n,a + 1));
+		return (check_prime(n, a + 1));
 }
 
 
 /**
- * * check_prime -> it check prime numbers
- * * @n: number
- * * @i: iterator
- * * Return: a 1 or 0
- * */
+ *check_prime -> it check prime numbers
+ *@n: number
+ *@i: iterator
+ *Return: a 1 or 0
+ **/
 int check_prime(int n, int i)
 {
 	if (n == 0)
 		return (0);
-	if (n < 0 )
+	if (n < 0)
 		return (0);
 	if (n == 1)
 		return (0);
-	return (check_prime(n,2));
+	return (check_prime(n, 2));
 }
