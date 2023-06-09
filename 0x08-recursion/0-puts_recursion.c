0b01cb81f336f624d8df1c832bf752d2c61235d3
BKHALEED <bashorunkhaleed@gmail.com>
2023-06-09 08:05:13 -0700

    recursion commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _puts_recursion - function like puts ();
- * @a: input
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string that prints out
  * Return: Always 0 (Success)
  */
-void _puts_recursion(char *s){
+void _puts_recursion(char *s)
+{
 	if (*s == '\0')
 		_putchar('\n');
-	else{
+	else
+	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
