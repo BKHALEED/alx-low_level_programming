418fd07d805fff25d553686812267e9c5659c459
BKHALEED <bashorunkhaleed@gmail.com>
2023-06-09 10:56:40 -0700

    recursion commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _evaluate - evaluate function sqrt
+ * @i: integers
+ * @n: integers
+ * Return: evaluate sqrt
+ */
+int _evaluate(int i, int n)
+{
+	if (n == 0 || n == 1)
+		return (n);
+	else if (i * i < n)
+		return (_evaluate(i = i, n));
+	else if (i * i == n) /* Condition base*/
+		return (i);
+	return (-1);
+	return (-1);
+}
+/**
+ * _sqrt_recursion - evaluate sqrt
+ * @n: integers
+ * Return: Sqrt_recursion
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (i < 0)
+		return (-1);
+	else
+	{
+		return (_evaluate(i, n));
+	}
+}
